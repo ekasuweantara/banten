f7897c8e165fe8654deaca58561144db359f3270
deviilingga <deviilingga@gmail.com>
2018-02-04 14:06:59 +0800

    coba

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,24 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data = array (
+			'nama' => 'Devi Lingga Pratiwi',
+			'alamat' => 'jimabaran',
+			'kampus' => 'unud'
+
+		);
+		$this->load->view('welcome_message',$data);
+
+	}
+
+	public function cetak($satu = 'devi',$dua = 'eka')
+	{
+	//	$this->load->view('welcome_message');
+		echo 'parameter pertama '.$satu.'<br />';
+		echo 'parameter dua '.$dua.'<br />';
+
 	}
 }
